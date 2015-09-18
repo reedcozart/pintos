0617eb633d3099fd067137a2dffd52dfed866121
gconan <gconan@utexas.edu>
2015-09-18 14:45:14 -0500

    added signatures to sysCall.c-Conan

--- userprog/syscall.c
+++ userprog/syscall.c
@@ -18,3 +18,55 @@ syscall_handler (struct intr_frame *f UNUSED)
   printf ("system call!\n");
   thread_exit ();
 }
+
+void halt(void){
+
+}
+
+void exit(int status){
+
+}
+
+pid_t exec(const char* cmd_line){
+
+}
+
+int wait(pid_t pid){
+
+}
+
+bool create(conts char* file, unsigned initial_size){
+
+}
+
+bool remove(const char* file){
+
+}
+
+int open(cont char* file){
+
+}
+
+int filesize(int fd){
+
+}
+
+int read(int fd, void* buffer, unsigned size){
+
+}
+
+int write(int fd, const void* buffer, unsigned size){
+
+}
+
+void seek(int fd, unsigned position){
+
+}
+
+unsigned tell(int fd){
+
+}
+
+void close(int fd){
+
+}
