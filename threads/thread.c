32a2517685069021cc68ce736cfcdfcef0b4a8c2
Stephen Arifin <stephen.arifin@gmail.com>
2015-09-21 12:05:41 -0500

    implemented close, tell, seek

--- threads/thread.c
+++ threads/thread.c
@@ -468,6 +468,9 @@ init_thread (struct thread *t, const char *name, int priority)
   strlcpy (t->name, name, sizeof t->name);
   t->stack = (uint8_t *) t + PGSIZE;
   t->priority = priority;
+#ifdef USERPROG
+	list_init(&t->file_descrips);		
+#endif
   t->magic = THREAD_MAGIC;
   list_push_back (&all_list, &t->allelem);
 }
--- userprog/syscall.c
+++ userprog/syscall.c
@@ -3,8 +3,13 @@
 #include <syscall-nr.h>
 #include "threads/interrupt.h"
 #include "threads/thread.h"
+#include "threads/synch.h"
 
 static void syscall_handler (struct intr_frame *);
+
+struct lock file_sys_lock;
+
+struct file_desc* get_fd(int fd);
 
 void
 syscall_init (void) 
@@ -60,13 +65,50 @@ int write(int fd, const void* buffer, unsigned size){
 }
 
 void seek(int fd, unsigned position){
-
+	lock_acquire(&file_sys_lock);
+	struct file_desc* filed = get_fd(fd);
+	if(filed && filed->file) {
+		file_seek(filed->file, position);
+	}
+	lock_release(&file_sys_lock);
 }
 
 unsigned tell(int fd){
-
+	lock_acquire(&file_sys_lock);
+	int result = -1;
+	struct file_desc* filed = get_fd(fd);
+	if(filed && filed->file) {
+		result = file_tell(filed);
+	}
+	lock_release(&file_sys_lock);
+	return result;
 }
 
 void close(int fd){
-
+	lock_acquire(&file_sys_lock);
+	struct file_desc* filed = get_fd(fd);
+	if(filed && filed->file) {
+		file_close(filed->file);
+		list_remove(&(filedd->elem));
+		free(fd);
+	}
+	lock_release(&file_sys_lock);
 }
+
+struct file_desc* get_fd(int fd) {
+	// Get thread list
+	struct thread* t = thread_current();
+
+	// Find the file descriptor 
+	struct list_elem* e = list_begin(&t->file_descrips);
+	while(e != list_end(&t->file_descrips) {
+		struct file_desc* d = list_entry(e, struct file_desc, elem);
+		if(d->id == fd) {
+			return d;
+		}
+		e = list_next(e);
+	}
+	return NULL;
+}
+
+
