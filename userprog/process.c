4ed0ea9e10ab3ac763ea6a618b85fa68d18f9ee9
Stephen Arifin <stephen.arifin@gmail.com>
2015-10-18 13:32:32 -0500

    process update

--- userprog/process.c
+++ userprog/process.c
@@ -410,8 +410,7 @@ load (const char *file_name, void (**eip) (void), void **esp)
                   zero_bytes = ROUND_UP (page_offset + phdr.p_memsz, PGSIZE);
                 }
                 printf("About to start load segment\n");
-              if (!load_segment (file, file_page, (void *) mem_page,
-                                 read_bytes, zero_bytes, writable))
+              if (!load_segment (file, file_page, (void *) mem_page, read_bytes, zero_bytes, writable))
                 printf("Load segment fails\n");
                 //goto done;
             }
