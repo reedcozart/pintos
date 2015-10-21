dd43abd2b2faa0ac17cc515c9fdbf6de443fdc58
faniel <fanielghirmay@utexas.edu>
2015-10-20 21:59:00 -0500

    infint swap

--- vm/swap.c
+++ vm/swap.c
@@ -21,7 +21,6 @@ static const size_t NUM_SECTORS_PER_PAGE = PGSIZE / BLOCK_SECTOR_SIZE;
 
 // Initialize the swapping mechanism
 void swap_init() {
-
 	// Initialize swap block with type BLOCK_SWAP
 	swap_block = block_get_role(BLOCK_SWAP);
 	if(swap_block == NULL) {
@@ -35,21 +34,23 @@ void swap_init() {
 
 void swap_read(int swap_page, const void* uaddr) {
 	int i;
-	printf("read block \n");
+	printf("read block swap page no %d  \n", swap_page);
+	printf("sector size %d \n", BLOCK_SECTOR_SIZE);
 	// Read through enough blocks to read a full page into swap_block
 	for(i = 0; i < NUM_SECTORS_PER_PAGE; i++) {
+		printf("reading at swap page %d and phy address %p \n", swap_page * NUM_SECTORS_PER_PAGE + i, uaddr + (i * BLOCK_SECTOR_SIZE));
 		block_read(swap_block, (swap_page * NUM_SECTORS_PER_PAGE) + i, uaddr + (i * BLOCK_SECTOR_SIZE));
 	}
-
 	bitmap_set(swap_free, swap_page, true);
 }
 
-void swap_write(const void* uaddr) {
+int swap_write(const void* uaddr) {
 	int swap_page = bitmap_scan(swap_free, 0, 1, true);
 	int i;
-	printf("write block \n");
+	printf("write block swap page no %d \n", swap_page);
 	// Write enough blocks for a full page into swap_block
 	for(i = 0; i < NUM_SECTORS_PER_PAGE; i++) {
+                printf("writing at swap page %d and phy address %p \n", swap_page * NUM_SECTORS_PER_PAGE + i, uaddr + (i * BLOCK_SECTOR_SIZE));
 		block_write(swap_block, (swap_page * NUM_SECTORS_PER_PAGE) + i, uaddr + (i * BLOCK_SECTOR_SIZE));
 	}
 
--- vm/swap.h
+++ vm/swap.h
@@ -3,7 +3,7 @@
 
 void swap_init();
 void swap_read(int swap_page, const void* uaddr);
-void swap_write(const void* uaddr);
+int swap_write(const void* uaddr);
 void swap_remove(int swap_page);
 
 #endif
